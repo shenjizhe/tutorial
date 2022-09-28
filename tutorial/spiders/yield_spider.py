53f1edec375055a181673a02f76446530dfb5b78
shenjizhe <shenjizhe@bfec.cn>
2022-09-28 10:44:38 +0800

    yield

--- tutorial/spiders/yield_spider.py
+++ tutorial/spiders/yield_spider.py
@@ -11,7 +11,7 @@ class YieldScrapy(scrapy.Spider):
     def parse(self, response):
         for quote in response.css("dic.quote"):
             yield {
-                'author': quote.css("small.author::text").get()
-                'text': quote.css("span.text::text").get()
+                'author': quote.css("small.author::text").get(),
+                'text': quote.css("span.text::text").get(),
                 'tags': quote.css("div.tags a.tag::text").getall()
             }
