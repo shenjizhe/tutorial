3f1c7a467e178fdb57423d34d2f98316a13c09fb
shenjizhe <shenjizhe@bfec.cn>
2022-09-28 13:28:35 +0800

    next page

--- tutorial/spiders/follow.py
+++ tutorial/spiders/follow.py
@@ -8,12 +8,12 @@ class FollowSpider(scrapy.Spider):
     ]
 
     def parse(self, response, **kwargs):
-        for quote in response.css('div.quote')
+        for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
             }
 
-            next_page = response.css('li.next a.attr(hred)').get()
+            next_page = response.css('li.next a.attr(href)').get()
             if next_page is not None:
                 yield response.follow(next_page, self.parse)
