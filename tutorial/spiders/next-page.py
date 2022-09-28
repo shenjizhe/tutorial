42166462cd37bc2dfca03d7adf162287d9064b0a
shenjizhe <shenjizhe@bfec.cn>
2022-09-28 13:07:39 +0800

    next page

--- tutorial/spiders/next-page.py
+++ tutorial/spiders/next-page.py
@@ -1,5 +1,6 @@
 import scrapy
 
+
 class NextPageSpider(scrapy.Spider):
     name = 'next-spider'
     start_urls = [
@@ -9,12 +10,12 @@ class NextPageSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         for quote in response.css('div.quote'):
-            yield{
+            yield {
                 'text': quote.css('span.text::text').get(),
-                'author':quote.css('small.author::text').get(),
-                'tags':quote.css('div.tags a.tag::text').getall(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
             }
-        nextpage = response.css('li.next a::attr(href)').get()
-        if(nextpage is not None):
-            nextpage = response.urljoin(nextpage)
-            yield scrapy.Request(nextpage,callback=self.parse)
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
