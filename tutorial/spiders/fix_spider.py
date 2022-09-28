f4cfe8056c55359b240515b903bb4e4b97fae4cd
shenjizhe <shenjizhe@bfec.cn>
2022-09-28 15:00:19 +0800

    mix spider

--- tutorial/spiders/fix_spider.py
+++ tutorial/spiders/fix_spider.py
@@ -8,17 +8,17 @@ class MixSpider(scrapy.Spider):
     ]
 
     def parse(self, response, **kwargs):
-        author_urls = response.css('.anthor +a')
+        author_urls = response.css('.author +a')
         yield from response.follow_all(author_urls, self.parse_author)
         urls = response.css('li.next a')
         yield from response.follow_all(urls, self.parse)
 
-    def parse_author(self, response, url):
+    def parse_author(self, response):
         def extract(query):
             return response.css(query).get(default='').strip()
 
         yield {
             'name': extract('h3.author-title::text'),
             'birth': extract('.author-born-date::text'),
-            'location': extract('.author-born-location::text'),
+            'description': extract('.author-description::text'),
         }
