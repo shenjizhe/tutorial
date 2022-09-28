b311f775c2507b65eae5eb9a995bbcc13167a147
shenjizhe <shenjizhe@bfec.cn>
2022-09-28 10:43:07 +0800

    yield

--- tutorial/spiders/yield_spider.py
+++ tutorial/spiders/yield_spider.py
@@ -1,15 +1,17 @@
 import scrapy
 
+
 class YieldScrapy(scrapy.Spider):
     name = "yield-spider"
     start_urls = [
-        "http://quotes.toscrape.com/page/1",
-        "http://quotes.toscrape.com/page/2",
+        "http://quotes.toscrape.com/page/1/",
+        "http://quotes.toscrape.com/page/2/",
     ]
 
     def parse(self, response):
         for quote in response.css("dic.quote"):
-            author = quote.css("small.author::text").get()
-            text = quote.css("span.text::text").get()
-            tags = quote.css("div.tags a.tag::text").get()
-            yield dict(author=author,text=text,tags=tags)
+            yield {
+                'author': quote.css("small.author::text").get()
+                'text': quote.css("span.text::text").get()
+                'tags': quote.css("div.tags a.tag::text").getall()
+            }
