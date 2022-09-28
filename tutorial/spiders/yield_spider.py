6ff19434c34929f71e9c11e599ffb78e70e7e4a4
shenjizhe <shenjizhe@bfec.cn>
2022-09-28 10:08:29 +0800

    yield

---
+++ tutorial/spiders/yield_spider.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class YieldScrapy(scrapy.Spider):
+    name = "yield-spider"
+    start_urls = [
+        "http://quotes.toscrape.com/page/1",
+        "http://quotes.toscrape.com/page/2",
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath("//div[@class=''quote]"):
+            author = quote.css("small.author::text").get()
+            text = quote.css("span.text::text").get()
+            tags = quote.css("div.tags a.tag::text").get()
+            yield dict(author=author,text=text,tags=tags)
